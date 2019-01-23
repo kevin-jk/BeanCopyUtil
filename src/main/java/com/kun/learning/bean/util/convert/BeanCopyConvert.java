96e0926f0936add4f0ec4c1e18b58602df348221
jiakun8 <jrjiakun@jd.com>
2019-01-23 14:23:21 +0800

    comment

--- src/main/java/com/kun/learning/bean/util/convert/BeanCopyConvert.java
+++ src/main/java/com/kun/learning/bean/util/convert/BeanCopyConvert.java
@@ -5,6 +5,8 @@ package com.kun.learning.bean.util.convert;
  *
  * J: 目标对象属性对应的类型
  * K: 源对象对应属性对应的类型
+ *
+ * 当转换对象的类型不一样的时候，用户自定义转换类型
  * */
 public interface BeanCopyConvert<J,K> {
     J convert(K src);
